450c6f0f129eae298701504825844fd087b16011
Felipe-Ferreira-Dev <felipe.ferreira.sco@gmail.com>
2021-10-25 20:10:42 +0000

    fmt

--- terraform-rede/main.tf
+++ terraform-rede/main.tf
@@ -4,8 +4,8 @@ provider "aws" {
 
 
 resource "aws_vpc" "vpc_desafio" {
-  cidr_block       = "10.10.0.0/22"
-  instance_tenancy = "default"
+  cidr_block           = "10.10.0.0/22"
+  instance_tenancy     = "default"
   enable_dns_hostnames = true
   enable_dns_support   = true
 
@@ -17,9 +17,9 @@ resource "aws_vpc" "vpc_desafio" {
 
 #=====================SUBNETS publicas
 resource "aws_subnet" "sb_desafio_1a" {
-  vpc_id            = aws_vpc.vpc_desafio.id
-  cidr_block        = "10.10.0.8/24"
-  availability_zone = "us-east-1a"
+  vpc_id                  = aws_vpc.vpc_desafio.id
+  cidr_block              = "10.10.0.8/24"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 
   tags = {
@@ -29,9 +29,9 @@ resource "aws_subnet" "sb_desafio_1a" {
 }
 
 resource "aws_subnet" "sb_desafio_1b" {
-  vpc_id            = aws_vpc.vpc_desafio.id
-  cidr_block        = "10.10.0.16/24"
-  availability_zone = "us-east-1b"
+  vpc_id                  = aws_vpc.vpc_desafio.id
+  cidr_block              = "10.10.0.16/24"
+  availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
 
   tags = {
@@ -42,11 +42,11 @@ resource "aws_subnet" "sb_desafio_1b" {
 
 #=====================SUBNET privada
 resource "aws_subnet" "sb_desafio_1c" {
-  vpc_id            = aws_vpc.vpc_desafio.id
-  cidr_block        = "10.10.0.16/24"
-  availability_zone = "us-east-1c"
+  vpc_id                  = aws_vpc.vpc_desafio.id
+  cidr_block              = "10.10.0.16/24"
+  availability_zone       = "us-east-1c"
   map_public_ip_on_launch = false
-  
+
   tags = {
     Name = "sb1c-pv-desafio-gama-um"
   }
@@ -68,12 +68,12 @@ resource "aws_eip" "nat_eip" {
 }
 
 resource "aws_nat_gateway" "ntg_desafio" {
-  allocation_id = "${aws_eip.nat_eip.id}"
+  allocation_id = aws_eip.nat_eip.id
   subnet_id     = aws_subnet.sb_desafio_1c.id
   depends_on    = [aws_internet_gateway.igw_desafio]
- 
+
   tags = {
-    Name        =  "ntg-desafio-gama-um"
+    Name = "ntg-desafio-gama-um"
   }
 }
 
@@ -130,13 +130,13 @@ resource "aws_route_table" "rt_pv__desafio" {
 }
 
 resource "aws_route_table_association" "1a" {
- 
+
   subnet_id      = aws_subnet.sb_desafio_1a.id
   route_table_id = aws_route_table.rt_desafio.id
 }
 
 resource "aws_route_table_association" "1b" {
- 
+
   subnet_id      = aws_subnet.sb_desafio_1b.id
   route_table_id = aws_route_table.rt_desafio.id
 }
