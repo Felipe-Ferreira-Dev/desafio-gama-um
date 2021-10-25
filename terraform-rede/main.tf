223f2470e0e4659f5b13503a8558a327b4bdb9bc
Felipe Ferreira <62390835+Felipe-Ferreira-Dev@users.noreply.github.com>
2021-10-25 17:29:51 -0300

    Update main.tf

--- terraform-rede/main.tf
+++ terraform-rede/main.tf
@@ -97,19 +97,8 @@ resource "aws_route_table" "rt_pb_desafio" {
 
   route = [
     {
-      carrier_gateway_id         = ""
       cidr_block                 = "0.0.0.0/0"
-      destination_prefix_list_id = ""
-      egress_only_gateway_id     = ""
       gateway_id                 = aws_internet_gateway.igw_desafio.id
-      instance_id                = ""
-      ipv6_cidr_block            = ""
-      local_gateway_id           = ""
-      nat_gateway_id             = ""
-      network_interface_id       = ""
-      transit_gateway_id         = ""
-      vpc_endpoint_id            = ""
-      vpc_peering_connection_id  = ""
     }
   ]
 
@@ -125,27 +114,17 @@ resource "aws_route_table" "rt_pv_desafio" {
 
 
   route = [
-    {
-      carrier_gateway_id         = ""
-      cidr_block                 = "0.0.0.0/0"
-      destination_prefix_list_id = ""
-      egress_only_gateway_id     = ""
-      gateway_id                 = ""
-      instance_id                = ""
-      ipv6_cidr_block            = ""
-      local_gateway_id           = ""
+    {cidr_block                 = "0.0.0.0/0"
       nat_gateway_id             = aws_nat_gateway.ntg_desafio.id
-      network_interface_id       = ""
-      transit_gateway_id         = ""
-      vpc_endpoint_id            = ""
-      vpc_peering_connection_id  = ""
     }
   ]
+
 
   tags = {
     Name = "rt-pv-desafio-gama-um"
   }
 }
+
 
 resource "aws_route_table_association" "a1" {
  
@@ -167,7 +146,5 @@ resource "aws_route_table_association" "c1" {
   subnet_id      = "${aws_subnet.sb_desafio_1c.id}"
   route_table_id = "${aws_route_table.rt_pv_desafio.id}"
 }
-
-
 
 
