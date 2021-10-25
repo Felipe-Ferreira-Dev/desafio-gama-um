e647b038e5aa4633a6e2e1d3d37c55264ab65231
Felipe Ferreira <62390835+Felipe-Ferreira-Dev@users.noreply.github.com>
2021-10-25 17:37:07 -0300

    Update main.tf

--- terraform-rede/main.tf
+++ terraform-rede/main.tf
@@ -91,14 +91,13 @@ resource "aws_nat_gateway" "ntg_desafio" {
 }
 
 
-resource "aws_route_table" "rt_pb_desafio" {
-  vpc_id = aws_vpc.vpc_desafio.id
-
+resource "aws_route_table" "publico" {
+  vpc_id = "${aws_vpc.vpc_desafio.id}"
 
   route = [
     {
       cidr_block                 = "0.0.0.0/0"
-      gateway_id                 = aws_internet_gateway.igw_desafio.id
+      gateway_id                 = "${aws_internet_gateway.igw_desafio.id}"
     }
   ]
 
@@ -109,13 +108,13 @@ resource "aws_route_table" "rt_pb_desafio" {
 }
 
 
-resource "aws_route_table" "rt_pv_desafio" {
-  vpc_id = aws_vpc.vpc_desafio.id
-
+resource "aws_route_table" "privado" {
+   vpc_id = "${aws_vpc.vpc_desafio.id}"
 
   route = [
-    {cidr_block                 = "0.0.0.0/0"
-      nat_gateway_id             = aws_nat_gateway.ntg_desafio.id
+    {
+        cidr_block                 = "0.0.0.0/0"
+        nat_gateway_id             = "${aws_nat_gateway.ntg_desafio.id}"
     }
   ]
 
@@ -129,22 +128,21 @@ resource "aws_route_table" "rt_pv_desafio" {
 resource "aws_route_table_association" "a1" {
  
   subnet_id      = "${aws_subnet.sb_desafio_a.id}"
-  route_table_id = "${aws_route_table.rt_pb_desafio.id}"
+  route_table_id = "${aws_route_table.publico.id}"
 }
 
 
 resource "aws_route_table_association" "b1" {
  
   subnet_id      = "${aws_subnet.sb_desafio_1b.id}"
-  route_table_id = "${aws_route_table.rt_pb_desafio.id}"
+  route_table_id = "${aws_route_table.publico.id}"
 }
-
 
 
 resource "aws_route_table_association" "c1" {
 
   subnet_id      = "${aws_subnet.sb_desafio_1c.id}"
-  route_table_id = "${aws_route_table.rt_pv_desafio.id}"
+  route_table_id = "${aws_route_table.privado.id}"
 }
 
 
