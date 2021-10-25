1a032d0b08ffc74a6ba188b967ed3cd22a5e7c50
Felipe Ferreira <62390835+Felipe-Ferreira-Dev@users.noreply.github.com>
2021-10-25 18:00:34 -0300

    Update main.tf

--- terraform-rede/main.tf
+++ terraform-rede/main.tf
@@ -2,21 +2,15 @@ provider "aws" {
   region = "us-east-1"
 }
 
-
-
 resource "aws_vpc" "vpc_desafio" {
   cidr_block       = "10.10.0.0/22"
   instance_tenancy = "default"
   enable_dns_hostnames = true
   enable_dns_support   = true
-
-
   tags = {
     Name = "vpc-desafio-gama-um"
   }
 }
-
-
 
 #=====================SUBNETS publicas
 resource "aws_subnet" "sb_desafio_1a" {
@@ -29,19 +23,15 @@ resource "aws_subnet" "sb_desafio_1a" {
   tags = {
     Name = "sb1a-pb-desafio-gama-um"
   }
-
-
 }
-
 
 resource "aws_subnet" "sb_desafio_1b" {
   vpc_id            = aws_vpc.vpc_desafio.id
   cidr_block        = "10.10.0.16/24"
   availability_zone = "us-east-1b"
   map_public_ip_on_launch = true
-
-
-  tags = {
+  
+  tags = {
     Name = "sb1b-pb-desafio-gama-um"
   }
 }
@@ -58,57 +48,47 @@ resource "aws_subnet" "sb_desafio_1c" {
   tags = {
     Name = "sb1c-pv-desafio-gama-um"
   }
-
-
 }
 #=====================SUBNET privada
 
-
 resource "aws_internet_gateway" "igw_desafio" {
   vpc_id = aws_vpc.vpc_desafio.id
-
-
   tags = {
     Name = "igw-desafio-gama-um"
   }
 }
-
 
 resource "aws_eip" "nat_eip" {
   vpc        = true
   depends_on = [aws_internet_gateway.igw_desafio]
 }
 
-
 resource "aws_nat_gateway" "ntg_desafio" {
   allocation_id = "${aws_eip.nat_eip.id}"
   subnet_id     = aws_subnet.sb_desafio_1c.id
   depends_on    = [aws_internet_gateway.igw_desafio]
- 
-tags = {
-Name = "ntg-desafio-gama-um"
-}
+  tags = {
+    Name = "ntg-desafio-gama-um"
+  }
 }
 
 resource "aws_route_table" "publico" {
-  vpc_id = "aws_vpc.vpc_desafio.id
+  vpc_id = aws_vpc.vpc_desafio.id
 
   route = [
     {
       cidr_block                 = "0.0.0.0/0"
-      gateway_id                 = "aws_internet_gateway.igw_desafio.id
+      gateway_id                 = aws_internet_gateway.igw_desafio.id
     }
   ]
-
-
-  tags = {
+tags = {
     Name = "rt-pb-desafio-gama-um"
   }
 }
 
 
 resource "aws_route_table" "privado" {
-  vpc_id = "aws_vpc.vpc_desafio.id
+  vpc_id = aws_vpc.vpc_desafio.id
 
   route = [
     {
