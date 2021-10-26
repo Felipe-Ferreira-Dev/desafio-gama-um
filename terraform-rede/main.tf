ff2b4cd4a69a7c2c12abef950083b0babc6bfdd1
Felipe Ferreira <62390835+Felipe-Ferreira-Dev@users.noreply.github.com>
2021-10-26 09:36:24 -0300

    ajustes route table

--- terraform-rede/main.tf
+++ terraform-rede/main.tf
@@ -74,6 +74,16 @@ resource "aws_route_table" "publico" {
     {
       cidr_block = "0.0.0.0/0"
       gateway_id = aws_internet_gateway.igw_desafio.id
+      destination_prefix_list_id = ""
+      egress_only_gateway_id     = ""
+      carrier_gateway_id         = ""
+      instance_id                = ""
+      ipv6_cidr_block            = ""
+      local_gateway_id           = ""
+      network_interface_id       = ""
+      transit_gateway_id         = ""
+      vpc_endpoint_id            = ""
+      vpc_peering_connection_id  = ""
     }
   ]
   tags = {
@@ -89,6 +99,16 @@ resource "aws_route_table" "privado" {
     {
       cidr_block     = "0.0.0.0/0"
       nat_gateway_id = aws_nat_gateway.ntg_desafio.id
+      destination_prefix_list_id = ""
+      egress_only_gateway_id     = ""
+      carrier_gateway_id         = ""
+      instance_id                = ""
+      ipv6_cidr_block            = ""
+      local_gateway_id           = ""
+      network_interface_id       = ""
+      transit_gateway_id         = ""
+      vpc_endpoint_id            = ""
+      vpc_peering_connection_id  = ""
     }
   ]
 
